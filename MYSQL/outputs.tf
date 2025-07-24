4c880875776bab8d76c681909ac9a08f0e94109d
Kaliyappan1 <yappank17@gmail.com>
2025-07-24 13:57:42 +0530

    mysql s3 output as changed

--- MYSQL/outputs.tf
+++ MYSQL/outputs.tf
@@ -3,10 +3,13 @@ output "instance_public_ip" {
 }
 
 output "pem_s3_url" {
-  value = length(aws_s3_object.upload_pem_key) > 0 ?
+  value = (
+    length(aws_s3_object.upload_pem_key) > 0 ?
     "s3://${aws_s3_object.upload_pem_key[0].bucket}/${aws_s3_object.upload_pem_key[0].key}" :
     "Key not uploaded (already exists in S3)"
+  )
 }
+
 
 
 output "instance_id" {
